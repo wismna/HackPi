7e556db1142b3b7152e6c808ec8375030061e20a
wismna <geogeob@hotmail.com>
2017-04-21 12:11:00 -0400

    Fingerprinting now only uses setup requests with an non-zero wIndex

--- fingerprint.sh
+++ fingerprint.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# s means Setup, 
-# 80 means device to host (bmRequestType), 
-# 06 means get descriptors (bRequest), 
+# Analyze USB Setup Request
+# 80 means device to host (bmRequestType)
+# 06 means get descriptors (bRequest)
 # 03xx means string descriptors (wValue)
-#WLENGTHS=`cat /tmp/0.mon.out | grep "s 80 06 03" | cut -d " " -f 10`
-WLENGTHS=`dmesg | grep "USB DWC2 REQ 80 06 03" | tee /home/pi/HackPi/usbreq.log | cut -d "]" -f 2 | cut -d " " -f 9`
+# 0409 means english (wIndex)
+# wLength is the size of the descriptor and this is what we want
+LOGFILE=/home/pi/HackPi/usbreq.log
+dmesg | grep "USB DWC2 REQ 80 06 03" | tee $LOGFILE
+WLENGTHS=`awk '$9!="0000" { print $10 }' $LOGFILE`
 TOTAL=0
 COUNTER=0
 for i in $WLENGTHS; do
     if [ "$i" = "00ff" ]; then
-        let COUNTER=COUNTER+1 
+        let COUNTER=COUNTER+1
     fi
     let TOTAL=TOTAL+1
     #echo wLength: $i
